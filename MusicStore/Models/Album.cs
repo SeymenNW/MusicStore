fbf349f09033660e803f0304524094c23ab6b8c4
SeymenNW <113972346+SeymenNW@users.noreply.github.com>
2023-09-06 12:18:00 +0200

    Add project files.

---
+++ MusicStore/Models/Album.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicStore.Models
+{
+    public class Album
+    {
+
+        public int AlbumId { get; set; }
+        public int GenreId { get; set; }
+        public int ArtistId { get; set; }
+        public double Price { get; set; }
+        public string AlbumArtUrl { get; set; }
+    }
+}
