de108fac1d4017837ad6384223fbefb8e82dc466
monaz19 <monaz19@student.sdu.com>
2023-09-06 12:48:57 +0200

    playlist controller

---
+++ MusicStore/Data/PlaylistContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MusicStore.Models;
+
+namespace MusicStore.Data
+{
+    public class PlaylistContext : DbContext
+    {
+        public PlaylistContext (DbContextOptions<PlaylistContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MusicStore.Models.Playlist> Playlist { get; set; } = default!;
+    }
+}
